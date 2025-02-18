e82dbed41255171d5d48a76c7220c53b50a4de46
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2025-02-18 15:54:45 +0100

    Update dependencies (#164)
    * Update minor:  cmd `npm install --save` + `eslint-plugin-react-refresh` + `typescript`
    * Update Deck.GL part
    * update deps
    * remove unnecessary `@mui/system`: we use `@mui/material` not base-ui.
    * dedup declaration
    * Fix & revert to legacy API
    
    Signed-off-by: Tristan Chuine <tristan.chuine_externe@rte-france.com>

--- src/components/network-map-viewer/network/draw-control.ts
+++ src/components/network-map-viewer/network/draw-control.ts
@@ -6,14 +6,18 @@
  */
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import { useCallback } from 'react';
-import { type ControlPosition, useControl } from 'react-map-gl';
-import { EventedListener } from 'mapbox-gl';
+import { type ControlPosition, useControl } from 'react-map-gl/mapbox-legacy';
+
+// type has been removed from react-map-gl or mapbox-gl
+type EventedListener = (event?: unknown) => unknown;
 
 let mapDrawerController: MapboxDraw | undefined = undefined;
 
 export function getMapDrawer() {
     return mapDrawerController;
 }
+
+const emptyFn = () => {};
 
 export enum DRAW_MODES {
     DRAW_POLYGON = 'draw_polygon',
@@ -26,17 +30,15 @@ export type DrawControlProps = ConstructorParameters<typeof MapboxDraw>[0] & {
     position?: ControlPosition;
     readyToDisplay: boolean;
     onDrawPolygonModeActive: (polygoneDrawing: DRAW_MODES) => void;
-    onCreate: EventedListener;
-    onUpdate: EventedListener;
-    onDelete: EventedListener;
+    onCreate?: EventedListener;
+    onUpdate?: EventedListener;
+    onDelete?: EventedListener;
 };
 
 export default function DrawControl(props: DrawControlProps) {
     const { onDrawPolygonModeActive } = props;
     const onModeChange = useCallback(
-        (e: { mode: DRAW_MODES }) => {
-            onDrawPolygonModeActive(e.mode);
-        },
+        (e: { mode: DRAW_MODES }) => onDrawPolygonModeActive(e.mode),
         [onDrawPolygonModeActive]
     );
 
@@ -48,28 +50,20 @@ export default function DrawControl(props: DrawControlProps) {
         },
         //on add
         ({ map }) => {
-            map.on('draw.create', props.onCreate);
-            map.on('draw.update', props.onUpdate);
-            map.on('draw.delete', props.onDelete);
+            map.on('draw.create', props.onCreate ?? emptyFn);
+            map.on('draw.update', props.onUpdate ?? emptyFn);
+            map.on('draw.delete', props.onDelete ?? emptyFn);
             map.on('draw.modechange', onModeChange);
         },
         //onRemove
         ({ map }) => {
-            map.off('draw.create', props.onCreate);
-            map.off('draw.update', props.onUpdate);
-            map.off('draw.delete', props.onDelete);
+            map.off('draw.create', props.onCreate ?? emptyFn);
+            map.off('draw.update', props.onUpdate ?? emptyFn);
+            map.off('draw.delete', props.onDelete ?? emptyFn);
             map.off('draw.modechange', onModeChange);
         },
-        {
-            position: props.position,
-        }
+        { position: props.position }
     );
 
     return null;
 }
-
-DrawControl.defaultProps = {
-    onCreate: () => {},
-    onUpdate: () => {},
-    onDelete: () => {},
-};
