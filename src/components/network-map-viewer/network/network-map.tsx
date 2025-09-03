daca6b8215ec570d7ce1adea6d2d2f7854e6cccf
sBouzols <sylvain.bouzols@gmail.com>
2025-09-03 16:29:34 +0200

    remove unused import
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/network-map-viewer/network/network-map.tsx
+++ src/components/network-map-viewer/network/network-map.tsx
@@ -50,7 +50,6 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 import {
     EQUIPMENT_TYPES,
-    type MapAnyLine,
     type MapAnyLineWithType,
     type MapEquipment,
     type MapHvdcLine,
