f76496d7c0ae2245cedab4d91d684ba4f690b797
BOUTIER Charly <charly.boutier@rte-france.com>
2025-09-29 11:12:54 +0200

    Sonar
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/components/network-area-diagram-viewer/network-area-diagram-viewer.ts
+++ src/components/network-area-diagram-viewer/network-area-diagram-viewer.ts
@@ -105,7 +105,7 @@ export class NetworkAreaDiagramViewer {
     static readonly ZOOM_CLASS_PREFIX = 'nad-zoom-';
 
     private randomId(): string {
-        const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
+        const uint32 = globalThis.crypto.getRandomValues(new Uint32Array(1))[0];
         return uint32.toString(16);
     }
 
