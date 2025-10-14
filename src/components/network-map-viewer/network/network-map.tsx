a8039ed9426e37844779fb820dd640ae352b225d
Franck LECUYER <franck.lecuyer@rte-france.com>
2025-10-14 17:13:26 +0200

    Fix bad sonar maintainability rating
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/components/network-map-viewer/network/network-map.tsx
+++ src/components/network-map-viewer/network/network-map.tsx
@@ -32,7 +32,6 @@ import { type Layer, type PickingInfo } from 'deck.gl';
 import type { Feature, Polygon } from 'geojson';
 import mapboxgl, { type MapLayerMouseEvent as MapBoxLayerMouseEvent } from 'mapbox-gl';
 import maplibregl, { type MapLayerMouseEvent as MapLibreLayerMouseEvent } from 'maplibre-gl';
-import PropTypes from 'prop-types';
 import {
     forwardRef,
     memo,
@@ -849,56 +848,6 @@ const NetworkMap = forwardRef<NetworkMapRef, NetworkMapProps>((rawProps, ref) =>
         )
     );
 });
-
-NetworkMap.propTypes = {
-    disabled: PropTypes.bool,
-    geoData: PropTypes.instanceOf(GeoData),
-    mapBoxToken: PropTypes.string,
-    mapEquipments: PropTypes.instanceOf(MapEquipments),
-    mapLibrary: PropTypes.oneOf([CARTO, CARTO_NOLABEL, MAPBOX, ETALAB]),
-    mapTheme: PropTypes.oneOf([LIGHT, DARK]),
-
-    areFlowsValid: PropTypes.bool,
-    arrowsZoomThreshold: PropTypes.number,
-    centerOnSubstation: PropTypes.any,
-    displayOverlayLoader: PropTypes.bool,
-    filteredNominalVoltages: PropTypes.array,
-    initialPosition: PropTypes.any,
-    initialZoom: PropTypes.number,
-    isManualRefreshBackdropDisplayed: PropTypes.bool,
-    labelsZoomThreshold: PropTypes.number,
-    lineFlowAlertThreshold: PropTypes.number,
-    lineFlowColorMode: PropTypes.oneOf(Object.values(LineFlowColorMode)),
-    lineFlowMode: PropTypes.oneOf(Object.values(LineFlowMode)),
-    lineFullPath: PropTypes.bool,
-    lineParallelPath: PropTypes.bool,
-    renderPopover: PropTypes.func,
-    tooltipZoomThreshold: PropTypes.number,
-    // With mapboxgl v2 (not a problem with maplibre), we need to call
-    // map.resize() when the parent size has changed, otherwise the map is not
-    // redrawn. It seems like this is autodetected when the browser window is
-    // resized, but not for programmatic resizes of the parent. For now in our
-    // app, only study display mode resizes programmatically
-    // use this prop to make the map resize when needed, each time this prop changes, map.resize() is trigged
-    triggerMapResizeOnChange: PropTypes.any,
-    updatedLines: PropTypes.array,
-    useName: PropTypes.bool,
-    visible: PropTypes.bool,
-    shouldDisableToolTip: PropTypes.bool,
-    locateSubStationZoomLevel: PropTypes.number,
-    enablePitchAndRotate: PropTypes.bool,
-    onHvdcLineMenuClick: PropTypes.func,
-    onLineMenuClick: PropTypes.func,
-    onTieLineMenuClick: PropTypes.func,
-    onManualRefreshClick: PropTypes.func,
-    onSubstationClick: PropTypes.func,
-    onSubstationClickChooseVoltageLevel: PropTypes.func,
-    onSubstationMenuClick: PropTypes.func,
-    onVoltageLevelMenuClick: PropTypes.func,
-    onDrawPolygonModeActive: PropTypes.func,
-    onPolygonChanged: PropTypes.func,
-    onDrawEvent: PropTypes.func,
-};
 
 //TODO why is the FunctionComponent memoized?! It never change and useMemo!=useCallback
 export default memo(NetworkMap);
