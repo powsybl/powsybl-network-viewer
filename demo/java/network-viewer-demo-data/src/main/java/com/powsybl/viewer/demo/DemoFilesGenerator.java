142f92f51dd008dce198816c87ef218be492ce69
Florian Dupuy <florian.dupuy@rte-france.com>
2025-12-16 16:04:00 +0100

    Add author and license header
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- demo/java/network-viewer-demo-data/src/main/java/com/powsybl/viewer/demo/DemoFilesGenerator.java
+++ demo/java/network-viewer-demo-data/src/main/java/com/powsybl/viewer/demo/DemoFilesGenerator.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.viewer.demo;
 
 import com.powsybl.cgmes.conformity.ReliCapGridCatalog;
@@ -26,6 +33,9 @@ import java.util.Properties;
 
 import static com.powsybl.nad.build.iidm.VoltageLevelFilter.NO_FILTER;
 
+/**
+ * @author Florian Dupuy {@literal <florian.dupuy at rte-france.com>}
+ */
 public final class DemoFilesGenerator {
 
     private DemoFilesGenerator() {
