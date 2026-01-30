1d21817bb31d51e35d81c4eb288d6f9b3cc43084
Christian Biasuzzi <christian.biasuzzi@soft.it>
2026-01-30 14:51:27 +0100

    [nad-viewer] fixes visible area (adaptiveTextZoom feature) (#305)
    [nad-viewer] fixes visible area (adaptiveTextZoom feature) (#305)
    
    * fixes adaptiveTextZoom computation of visible area
    * move computeVisibleArea to svg-utils; adds unit tests for it
    * use proper container' size for the computeVisibleArea function
    
    ---------
    
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@soft.it>

--- src/components/network-area-diagram-viewer/network-area-diagram-viewer.ts
+++ src/components/network-area-diagram-viewer/network-area-diagram-viewer.ts
@@ -1670,7 +1670,8 @@ export class NetworkAreaDiagramViewer {
     }
 
     private getElementsInViewbox(tolerance = 0) {
-        const viewBox = this.getViewBox();
+        const containerRect = this.container.getBoundingClientRect();
+        const viewBox = SvgUtils.computeVisibleArea(this.getViewBox(), containerRect.width, containerRect.height);
         const metadata = this.diagramMetadata;
         if (!viewBox || !metadata) {
             return { nodes: [], edges: [] };
