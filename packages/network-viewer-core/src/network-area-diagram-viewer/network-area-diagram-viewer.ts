664e58b74140090ae5274227900d9f6a43c89d8d
Christian Biasuzzi <christian.biasuzzi@soft.it>
2026-02-13 15:17:11 +0100

    fixes updating the middle edge arrow when moving a voltage level
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@soft.it>

--- packages/network-viewer-core/src/network-area-diagram-viewer/network-area-diagram-viewer.ts
+++ packages/network-viewer-core/src/network-area-diagram-viewer/network-area-diagram-viewer.ts
@@ -1434,8 +1434,9 @@ export class NetworkAreaDiagramViewer {
     }
 
     private updateEdgeLabel(edgeInfo: EdgeInfoMetadata, halfEdge1: HalfEdge | null, halfEdge2: HalfEdge | null) {
-        const positionElement: SVGGraphicsElement | null = this.getEdgeInfo(edgeInfo.svgId) as SVGGraphicsElement;
+        if (!halfEdge1 && !halfEdge2) return;
 
+        const positionElement: SVGGraphicsElement | null = this.getEdgeInfo(edgeInfo.svgId) as SVGGraphicsElement;
         if (!positionElement) return;
 
         let anchorPoint = new Point(0, 0);
@@ -1453,10 +1454,19 @@ export class NetworkAreaDiagramViewer {
 
         // move edge name position
         positionElement.setAttribute('transform', 'translate(' + DiagramUtils.getFormattedPoint(anchorPoint) + ')');
-        const angleElement: SVGGraphicsElement | null = positionElement.querySelector('text') as SVGGraphicsElement;
-        if (angleElement != null) {
+        const textElements = positionElement.querySelectorAll('text');
+        for (const textElement of textElements) {
             // change edge name angle
-            angleElement.setAttribute('transform', 'rotate(' + DiagramUtils.getFormattedValue(edgeNameAngle) + ')');
+            textElement.setAttribute('transform', 'rotate(' + DiagramUtils.getFormattedValue(edgeNameAngle) + ')');
+        }
+
+        // rotate arrow if it exists
+        if (edgeInfo.direction) {
+            const arrowElement = positionElement.querySelector('path');
+            if (arrowElement) {
+                const arrowAngle = HalfEdgeUtils.getMiddleArrowRotation(halfEdge1, halfEdge2, edgeInfo.direction);
+                arrowElement.setAttribute('transform', `rotate(${DiagramUtils.getFormattedValue(arrowAngle)})`);
+            }
         }
     }
 
