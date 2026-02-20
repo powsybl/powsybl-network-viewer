cf8118f2ce0680ba697a0c61ac2b2a1f27ea88e7
Nicolas Rol <nicolas.rol@rte-france.com>
2026-02-20 17:24:27 +0100

    Rewrite tests to use fast-xml-parser
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- packages/network-viewer-core/src/network-area-diagram-viewer/svg-writer.test.ts
+++ packages/network-viewer-core/src/network-area-diagram-viewer/svg-writer.test.ts
@@ -6,53 +6,14 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import '../../../../global.d.ts';
 import IEE14CdfNetworkMetadata from '../resources/test-data/nad-ieee14cdf-solved_metadata.json';
 
-import { readFileSync } from 'fs';
-import { join } from 'path';
 import { SvgWriter } from './svg-writer';
+import { getSvgFromFile } from './test-utils';
 
 test('testIEE14CdfNetwork', () => {
-    const expectedSvg = normalizeXml(getSvgFromFile('../resources/test-data/nad-ieee14cdf-solved.svg'));
-    const createdSvg = normalizeXml(new SvgWriter(IEE14CdfNetworkMetadata).getSvg());
-    expect(createdSvg).toBe(expectedSvg);
+    const actual = new SvgWriter(IEE14CdfNetworkMetadata).getSvg();
+    const expected = getSvgFromFile('../resources/test-data/nad-ieee14cdf-solved.svg');
+    expect(actual).toEqualSvg(expected, { epsilon: 1e-3 });
 });
-
-function getSvgFromFile(file: string): string {
-    const filePath = join(__dirname, file);
-    const fileContent = readFileSync(filePath, 'utf8');
-    return fileContent;
-}
-
-function normalizeXml(xml: string): string {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(xml, 'text/xml');
-    removeWhitespaceNodes(doc);
-    sortAttributes(doc);
-    return new XMLSerializer().serializeToString(doc);
-}
-
-function removeWhitespaceNodes(node: Node): void {
-    for (let i = node.childNodes.length - 1; i >= 0; i--) {
-        const child = node.childNodes[i];
-        if (child.nodeType === Node.TEXT_NODE && !child.nodeValue?.trim()) {
-            node.removeChild(child);
-        } else if (child.nodeType === Node.ELEMENT_NODE) {
-            removeWhitespaceNodes(child);
-        }
-    }
-}
-
-function sortAttributes(doc: XMLDocument) {
-    const walker = doc.createTreeWalker(doc, NodeFilter.SHOW_ELEMENT);
-    while (walker.nextNode()) {
-        const el = walker.currentNode as Element;
-        const attrs = Array.from(el.attributes).sort((a, b) => a.name.localeCompare(b.name));
-        for (const attr of attrs) {
-            el.removeAttributeNode(attr);
-        }
-        for (const attr of attrs) {
-            el.setAttributeNode(attr);
-        }
-    }
-}
--- setupTests.ts
+++ setupTests.ts
@@ -5,9 +5,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+import { toEqualSvg } from './packages/network-viewer-core/src/network-area-diagram-viewer/svgMatcher';
+
+expect.extend({ toEqualSvg });
+
 //FIXME workaround svg.panzoom.js import crash even though it's not used
 /* eslint-disable */
-declare var SVG: any;
-global.SVG = () => {};
-global.SVG.extend = () => {};
+(global as any).SVG = () => {};
+(global as any).SVG.extend = () => {};
 /* eslint-enable */
