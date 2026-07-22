4e2314c8a0ccdd18e2125e342f2f14eb2efc666f
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-07-22 15:18:11 +0200

    complete test for completeSubstationsInfos
    Signed-off-by: Samir Romdhani <samir.romdhani_externe@rte-france.com>

--- packages/network-map-layers/src/network/map-equipments.test.ts
+++ packages/network-map-layers/src/network/map-equipments.test.ts
@@ -12,17 +12,43 @@ import { describe } from 'vitest';
 
 describe('Test MapEquipments', () => {
     test('completeSubstationsInfos with undefined should keep map empty', () => {
+        // Given
         const equipment: MapEquipments = new MapEquipments();
-        const substation1: MapSubstation = { id: 's0', voltageLevels: [] };
-        // When MapSubstation
+        const substation1: MapSubstation = {
+            id: 's0',
+            voltageLevels: [{ id: 'v0', nominalV: 400, substationId: 's0' }],
+        };
+        const substation2: MapSubstation = {
+            id: 's1',
+            voltageLevels: [{ id: 'v1', nominalV: 220, substationId: 's1' }],
+        };
+        // When
         equipment.completeSubstationsInfos([substation1]);
         // Then
         expect(equipment.substationsById.size).toBe(1);
+        expect(equipment.voltageLevelsById.size).toBe(1);
         expect(equipment.substationsById.has('s0')).toBe(true);
+        expect(equipment.nominalVoltages).toStrictEqual([400]);
 
-        // When undefined MapSubstation
+        // When
+        equipment.completeSubstationsInfos([substation2]);
+        // Then
+        expect(equipment.substationsById.size).toBe(2);
+        expect(equipment.voltageLevelsById.size).toBe(2);
+        expect(equipment.substationsById.has('s0')).toBe(true);
+        expect(equipment.substationsById.has('s1')).toBe(true);
+        expect(equipment.nominalVoltages).toStrictEqual([400, 220]);
+
+        // When undefined substations
         equipment.completeSubstationsInfos(undefined);
         // Then
         expect(equipment.substationsById.size).toBe(0);
+        expect(equipment.voltageLevelsById.size).toBe(0);
+
+        // When empty substations
+        equipment.completeSubstationsInfos([]);
+        // Then
+        expect(equipment.substationsById.size).toBe(0);
+        expect(equipment.voltageLevelsById.size).toBe(0);
     });
 });
